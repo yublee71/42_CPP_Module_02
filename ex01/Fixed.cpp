22c2c03d0763a15d4b6bca8e4752eb8f4cd7a6b2
Yubeen Lee <lybin71@naver.com>
2025-07-14 21:54:14 +0100

    feat: ex01 done

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,15 +1,27 @@
 #include "Fixed.hpp"
 
+#include <cmath>
 #include <iostream>
 
 Fixed::Fixed() : fp_value_(0) {
   std::cout << "Default constructor called" << std::endl;
 }
 
+Fixed::Fixed(const int nb_int) : fp_value_(nb_int << fractional_bits_) {
+  std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float nb_fp)
+    : fp_value_(roundf(nb_fp * (1 << fractional_bits_))) {
+  std::cout << "Float constructor called" << std::endl;
+}
+
 Fixed::Fixed(const Fixed &other) {
   std::cout << "Copy constructor called" << std::endl;
   *this = other;
 }
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
 
 Fixed &Fixed::operator=(const Fixed &other) {
   std::cout << "Copy assignment operator called" << std::endl;
@@ -19,11 +31,17 @@ Fixed &Fixed::operator=(const Fixed &other) {
   return *this;
 }
 
-Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
-
-int Fixed::getRawBits(void) const {
-  std::cout << "getRawBits member function called" << std::endl;
-  return fp_value_;
-}
+int Fixed::getRawBits(void) const { return fp_value_; }
 
 void Fixed::setRawBits(int const raw) { fp_value_ = raw; }
+
+float Fixed::toFloat(void) const {
+  return (float)fp_value_ / (1 << fractional_bits_);
+}
+
+int Fixed::toInt(void) const { return fp_value_ >> fractional_bits_; }
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed) {
+  os << fixed.toFloat();
+  return os;
+}
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -1,9 +1,13 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed {
 public:
   Fixed();
+  Fixed(const int nb_int);
+  Fixed(const float nb_fp);
   Fixed(const Fixed &other);
   ~Fixed();
 
@@ -12,9 +16,14 @@ public:
   int getRawBits(void) const;
   void setRawBits(int const raw);
 
+  float toFloat(void) const;
+  int toInt(void) const;
+
 private:
   int fp_value_;
   static const int fractional_bits_ = 8;
 };
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
 
 #endif
